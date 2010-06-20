e840f47f3e29a596df60fd605657e28475e31d55
toritaba <toritaba@gmail.com>
2010-06-21 03:15:13 +0530

    Quick and Dirty Version

--- lib/string_to_pinyin.rb
+++ lib/string_to_pinyin.rb
@@ -1,5 +1,14 @@
 class String
   def to_pinyin
+    h = Hash.new
+    open('../data/idx99.txt','r').each do |line|
+      a = line.gsub("\n","").split("\t")
+      h.store a[0], a[1]
+    end
+    if h[self]
+      return h[self]
+    else
+      return nil
+    end
   end
 end
-
